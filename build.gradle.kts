aeab62d88ddb0bdd0ee12c5e02be62efe1caa757
Murphy <tester_qaster@test.com>
2024-09-12 13:47:55 +0300

    update dependancies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/io.rest-assured/rest-assured
     testImplementation("io.rest-assured:rest-assured:5.5.0")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    testImplementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
 }
 
 tasks.test {
